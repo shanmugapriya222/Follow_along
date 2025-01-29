1f9258d30a07618caca7bcfa18c16679eae9d1d6
shanmugapriya222 <shanmugapriyab211@gmail.com>
2025-01-29 14:10:45 +0530

    commited

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,4 +1,5 @@
 import LoginPage from './pages/Login'
 import SignupPage from './pages/Signup'
-import Home from './pages/home'
-export {LoginPage,SignupPage,Home};
+import Home from "./pages/Home";
+import CreateProduct from './pages/createProduct.jsx';
+export {LoginPage,SignupPage,Home,CreateProduct};
